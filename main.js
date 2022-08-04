355286e588839ef2d430318ff2be723c6dc4dc33
aleite <92058852+analeit@users.noreply.github.com>
2022-08-04 10:51:36 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,100 @@
+// JAVASCRIPT FILE 
+
+// verificar se JS está conectado
+console.log ('on');
+
+function displayHome() {
+    document.getElementById("home").style.display = "block";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("project-1").style.display = "none";
+    document.getElementById("project-2").style.display = "none";
+    document.getElementById("project-3").style.display = "none";
+    document.getElementById("project-4").style.display = "none";
+    document.getElementById("project-5").style.display = "none";
+    document.getElementById("project-6").style.display = "none";
+    $(window).scrollTop(0);
+}
+
+function displayProjects() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("projects").style.display = "block";
+    document.getElementById("project-1").style.display = "none";
+    document.getElementById("project-2").style.display = "none";
+    document.getElementById("project-3").style.display = "none";
+    document.getElementById("project-4").style.display = "none";
+    document.getElementById("project-5").style.display = "none";
+    document.getElementById("project-6").style.display = "none";
+    $(window).scrollTop(0);
+}
+
+function displayProject1() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("project-1").style.display = "block";
+    document.getElementById("project-2").style.display = "none";
+    document.getElementById("project-3").style.display = "none";
+    document.getElementById("project-4").style.display = "none";
+    document.getElementById("project-5").style.display = "none";
+    document.getElementById("project-6").style.display = "none";
+    $(window).scrollTop(0);
+}
+
+function displayProject2() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("project-1").style.display = "none";
+    document.getElementById("project-2").style.display = "block";
+    document.getElementById("project-3").style.display = "none";
+    document.getElementById("project-4").style.display = "none";
+    document.getElementById("project-5").style.display = "none";
+    document.getElementById("project-6").style.display = "none";
+    $(window).scrollTop(0);
+}
+
+function displayProject3() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("project-1").style.display = "none";
+    document.getElementById("project-2").style.display = "none";
+    document.getElementById("project-3").style.display = "block";
+    document.getElementById("project-4").style.display = "none";
+    document.getElementById("project-5").style.display = "none";
+    document.getElementById("project-6").style.display = "none";
+    $(window).scrollTop(0);
+}
+
+function displayProject4() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("project-1").style.display = "none";
+    document.getElementById("project-2").style.display = "none";
+    document.getElementById("project-3").style.display = "none";
+    document.getElementById("project-4").style.display = "block";
+    document.getElementById("project-5").style.display = "none";
+    document.getElementById("project-6").style.display = "none";
+    $(window).scrollTop(0);
+}
+
+function displayProject5() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("project-1").style.display = "none";
+    document.getElementById("project-2").style.display = "none";
+    document.getElementById("project-3").style.display = "none";
+    document.getElementById("project-4").style.display = "none";
+    document.getElementById("project-5").style.display = "block";
+    document.getElementById("project-6").style.display = "none";
+    $(window).scrollTop(0);
+}
+
+function displayProject6() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("project-1").style.display = "none";
+    document.getElementById("project-2").style.display = "none";
+    document.getElementById("project-3").style.display = "none";
+    document.getElementById("project-4").style.display = "none";
+    document.getElementById("project-5").style.display = "none";
+    document.getElementById("project-6").style.display = "block";
+    $(window).scrollTop(0);
+}
